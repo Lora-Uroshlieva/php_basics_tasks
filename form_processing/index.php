217c93eff2392b9568a659ea520af423c487e93c
lora.strizhakova@gmail.com <lora.strizhakova@gmail.com>
2017-04-05 12:09:08 +0300

    Added possibility to write comments in txt-file. Added possibility to read  comments from file and publish them on page. T[t file was changed to json. Validation of data from writing html-tags was added.

--- form_processing/index.php
+++ form_processing/index.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html>
+<?php
+
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Comments</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container" style="margin-top: 30px">
@@ -11,11 +16,12 @@
             <div class="form-group">
                 <label for="username">Username</label>
                 <input type="text" class="form-control" id="username" name="username" placeholder="Username">
-            </div>
+            </div><br/>
             <div class="form-group">
                 <label for="comment">Comment</label>
-                <textarea name="comment" id="comment" class="form-control"></textarea>
-            </div>
+                <textarea name="comment" id="comment" class="form-control" rows="10"></textarea>
+            </div><br/>
+            <input type="hidden" name="token" value="">
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
 
@@ -24,19 +30,67 @@
 </html>
 
 <?php
-var_dump($_POST);
+$content = file_get_contents(__DIR__.'/data/comments.json');
+$comments = explode('.PHP_EOL', $content);
+?>
+
+
+<?php foreach ($comments as $comment): ?>
+    <?php
+        if ($comment === '') {
+            continue;
+        }
+        list($username, $text) = explode('|', $comment);
+    ?>
+    <div>
+        <hr>
+        <p><b>User:</b> <?php echo $username; ?></p>
+        <p><b>Text of comment:</b> <?php echo $text; ?></p>
+    </div>
+<?php endforeach ?>
+
+
+
+<?php
 
 if (!empty($_POST)) {
-    $username = $_POST['username'];
-    $text = $_POST['comment'];
-    $data = "{$username}|{$text}.PHP_EOL";
-    var_dump('<br>', $data);
-    $path = __DIR__.'/data/comments.txt';
-    var_dump($path);
-    $handle = fopen($path, 'a');
-    $a = fwrite($handle, $data);
-    var_dump($a);
-    fclose($handle);
-};
+    $username = strip_tags($_POST['username']);
+    $text = strip_tags($_POST['comment']);
+
+    $fileName = 'comments1.json';
+    $pathToFile = 'data/data1';
+    if (!is_dir($pathToFile)) {
+        mkdir($pathToFile, $recursive=true);
+    }
+
+    $path = __DIR__.DIRECTORY_SEPARATOR.$pathToFile.DIRECTORY_SEPARATOR.$fileName;
+    if (!file_exists($path)) {
+        $fp = fopen($path, 'r');
+        fclose($fp);
+    }
+
+    $jsonContent = file_get_contents($path);
+    $comments = json_decode($jsonContent);
+    $comments[] = [$username, $text];
+    file_put_contents($path, json_encode($comments));
+
+
+
+
+
+//    $data = "{$username}|{$text}.PHP_EOL";
+//
+//
+//
+//    $isWritten = file_put_contents($path, $data, $flags=FILE_APPEND);
+//    $handler = fopen($path, 'a');  //another way to write in file
+//    $a = fwrite($handler, $data);
+//    fclose($handler);
+
+
+//    if ($isWritten) {
+//    echo "<p>Your comment is very important for us!</p>";
+//    }
+}
 
 ?>
