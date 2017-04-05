e970ceed3a2c63a01115dc881625421e1329e344
lora.strizhakova@gmail.com <lora.strizhakova@gmail.com>
2017-04-05 17:34:19 +0300

    Function of replacing bad_words before publication of comment was created.
    Function of getting full path to file with comments and creating file recursive doesn-t work. To fix!!!

--- form_processing/comments_view.php
+++ form_processing/comments_view.php
@@ -1,6 +1,13 @@
 <?php
 $content = file_get_contents(__DIR__.'/data/comments.txt');
 $comments = explode('.PHP_EOL', $content);
+
+function replaceBadWords (string $text) {
+    $dictionaryContent = file_get_contents(__DIR__.'/data/bad_words.txt'); //read bad words from file
+    $badWords = explode(PHP_EOL, $dictionaryContent); //get the list of words
+    $changed = str_ireplace($badWords, '###', $text); // check text and replace bad words, if were found.
+    return $changed;
+}
 ?>
 
 <h2>Comments</h2>
@@ -10,7 +17,11 @@ $comments = explode('.PHP_EOL', $content);
     if ($comment === '') {
         continue;
     }
+
     list($username, $text) = explode('|', $comment);
+
+    $text = replaceBadWords($text);
+
     ?>
     <div>
         <hr>
